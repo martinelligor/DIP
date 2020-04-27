f877904c022a2794ccaf0195c9155b0c179ce37d
Igor Martinelli <martinelligor@ip-192-168-31-179.ec2.internal>
2020-04-27 16:09:49 -0300

    Adding final version and notebook

--- Assignment 2/assignment_2.py
+++ Assignment 2/assignment_2.py
@@ -19,7 +19,7 @@ def read_input():
 
     """
     filename = str(input()).rstrip() 
-    r = imageio.imread('../input_and_output_imgs/'+filename) 
+    r = imageio.imread('./input_and_output_imgs/'+filename) 
     
     M = int(input())
     S = int(input())
@@ -33,16 +33,64 @@ def read_input():
     return r, M, S, n, param_1, param_2
 
 def gaussian_kernel(x, sigma):
+    """
+        This funcion applies the gaussian kernel function given a point x and a sigma.
+
+        Parameters
+        ----------
+            x: float
+                A parameter of the gaussian kernel.
+
+            sigma: float
+                The standard deviation of gaussian kernel.
+
+        Returns
+        -------
+            The value of the gaussian kernel applied on x, with std equals sigma.
+    """
     return (1/(2*np.pi*np.square(sigma)))*np.exp(-(np.square(x)/(2*np.square(sigma))))
 
 def scaling(I):
-    return ((I-I.min())*255)/I.max()
+    """
+        This function realizes the normalization operation in an image to transform it's pixels into 0-255 range.
+
+        Parameters
+        ----------
+            I: (numpy.array)
+                The image before the normalization.
+
+        Returns
+        -------
+            The normalized image between 0-255 range.
+    """
+    return (((I-I.min())*255.0)/(I.max()))
 
 def bilateral_filter(I, n, sigma_s, sigma_r):
+    """
+        This function is responsible to apply the biletarel filtering on a given image I.
+
+        Parameters
+        ----------
+            I: (numpy.array)
+                A matrix representing image I given as input.
+
+            n: int
+                The size of the filter used on the convolution operation.
+
+            sigma_s and sigma_r: float
+                The parameters used in gaussian kernel function.
+
+        Returns
+        -------
+            f: (numpy.array)
+                The result image after the operations in the input image I.
+    """
     factor = int(n/2)
     img_shape = I.shape
     f = np.zeros(img_shape)
+    # padding image to apply the filter.
     I = np.pad(I, pad_width=factor, mode='constant', constant_values=0)
+    # calculating the spatial component.
     euclidean_matrix =[np.sqrt(np.square(i-factor)+np.square(j-factor)) for (i,j) in np.ndindex(n,n)]
     g_s = np.vectorize(gaussian_kernel)(euclidean_matrix, sigma_s).reshape(n,n)
     
@@ -59,15 +107,42 @@ def bilateral_filter(I, n, sigma_s, sigma_r):
     return f
 
 def get_kernel(kernel_op):
+    """
+        This function is only responsible to return the choosed kernel to the method 2.
+
+        Parameters
+        ----------
+            kernel_op: int
+                The option selected as input.
+
+        Returns
+        -------
+            (numpy.array)
+                The right kernel given an option.
+    """
     if(kernel_op == 1):
         return np.array([[0,-1,0],[-1,4,-1],[0,-1,0]])
     else:
         return np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 
 def laplacian_filter(r, c, kernel_op):
+    """
+        This function is responsible to apply the laplacian filter in the image r given as input.
+
+        Parameters
+        ----------
+            r: (numpy.array)
+                An image given as input.
+
+            c: int
+                A constant used to multiply matrix after the operation of convolution.
+
+            kernel_op: int
+                The kernel that is choosed to realize the convolution operation.
+    """
     n = 1
-    I_f = r.copy()
-    kernel = get_kernel(kernel_op)
+    I_f = r.copy().astype(np.float)
+    kernel = np.flip(np.flip(get_kernel(kernel_op), 0), 1)
     I = np.pad(r, pad_width=n, mode='constant', constant_values=0).astype(np.float)
 
     for(i, j) in np.ndindex(r.shape):
@@ -75,17 +150,44 @@ def laplacian_filter(r, c, kernel_op):
 
     f = scaling(np.multiply(c, scaling(I_f)) + r)
 
-    return f.astype(np.uint8)
+    return f
 
 def vignette_filter(r, sigma_row, sigma_col):
+    """
+        This function is responsible to apply the operation of vignetting on the image r given as input.
+
+        Parameters
+        ----------
+            r: (numpy.array)
+                The image r given as input.
+
+            sigma_row and sigma_col: float
+                The parameters of sigma to apply the gaussian kernel in the rows and columns.
+
+        Returns
+        -------
+            The image after the vignetting operation.
+    """
+    # applying the operations in the columns.
     W_col = np.asmatrix(np.vectorize(gaussian_kernel)(np.arange(-int(r.shape[0]/2), int(r.shape[0]/2)+1, 1), sigma_row)[:r.shape[0]])
+    # applying the operations in the rows.
     W_row = np.asmatrix(np.vectorize(gaussian_kernel)(np.arange(-int(r.shape[1]/2), int(r.shape[1]/2)+1, 1), sigma_col)[:r.shape[1]])
 
-    return scaling(np.multiply(np.matmul(W_col.T, W_row), r)).astype(np.uint8)
+    # applying the operations to realized the vignetting operation.
+    f = scaling(np.multiply(np.matmul(W_col.T, W_row), r))
+    
+    return f
 
-def apply_image_transformation():
-    r, M, S, n, param_1, param_2 = read_input()
+def apply_image_transformation(r, M, S, n, param_1, param_2):
+    """
+        This function is responsible to redirect the operation in image r, given the method M, choosed in input.
 
+        Returns
+        -------
+            r, f: (numpy.array)
+                The original and the transformed image after the operations.
+    """
+    
     if(M == 1):
         f = bilateral_filter(r, n, param_1, param_2)
     elif(M == 2):
@@ -103,6 +205,8 @@ def apply_image_transformation():
     # printing value of the comparison in the screen.
     print(np.round(RSE, 4))
 
+    return r, f
+    
 def compare_images(reference_image, transformed_image):
     """
         This function realizes de comparison between the reference image and the transformed image.
@@ -125,4 +229,5 @@ def compare_images(reference_image, transformed_image):
     return RSE
 
 if __name__ == "__main__":
-    apply_image_transformation()
+    r, M, S, n, param_1, param_2 = read_input()
+    apply_image_transformation(r, M, S, n, param_1, param_2)
